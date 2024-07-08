def2b31bde81b659ba05684291b6eb3b0cf8130b
Lev <leozolotukhin@gmail.com>
2024-07-08 17:29:34 +0200

    new push

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/** @format */
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-setupCounter(document.querySelector('#counter'))
+const bebra = async () => {
+	return new Promise((resolve, reject) => {
+		const date = new Date();
+		const date2 = new Date("2004-07-27T11:45:00");
+		const age = date - date2;
+		const ageInYears = age / (1000 * 60 * 60 * 24 * 365.25);
+		const fullAge = Math.floor(ageInYears);
+
+		resolve(fullAge);
+	});
+};
+
+bebra()
+	.then((fullAge) => {
+		const dateHTML = document.getElementById("cifer");
+		dateHTML.textContent = `27.07.2004 (${fullAge} Jahre alt)`;
+	})
+	.catch((error) => {
+		console.error(error);
+	});
